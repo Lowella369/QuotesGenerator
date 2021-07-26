9fff7675661e762fc2d70cc38d57bb98e00c2749
Lowella369 <lh850210@gmail.com>
2021-07-25 19:19:47 -0500

    Adds files

---
+++ js/quotesGenerator.js
@@ -0,0 +1,34 @@
+
+var data;
+
+let button = document.getElementById('quoteButton');
+let quote = document.getElementById('quote');
+let author = document.getElementById('author');
+  
+const displayQuote = () => {
+  let index = Math.floor(Math.random() * data.length);
+  quote.innerHTML = data[index].text;
+  let resAuthor = data[index].author;
+
+  if(!resAuthor){
+    author.innerHTML = "- Anonymous -";
+  } else {
+    author.innerHTML = "- " +data[index].author + " -";
+  }
+  
+}
+
+async function fetchQuotes() {
+  const URL = `https://type.fit/api/quotes`;
+  try {
+    const fetchResult = fetch(URL)
+    const response = await fetchResult;
+    data = await response.json();
+    displayQuote();
+  } catch(e){
+    throw Error(e);
+  }
+}
+ 
+fetchQuotes();
+button.addEventListener('click', fetchQuotes);
